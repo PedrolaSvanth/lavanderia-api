ef9e4e1c54bc0d1daf96cd5e94371875bf41c12e
PedrolaSvanth <pedro1pepecraft@gmail.com>
2026-06-09 17:38:01 -0300

    Iniciação do projeto da API da lavanderia

---
+++ src/test/java/br/com/lavanderia/lavanderia_api/LavanderiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lavanderia.lavanderia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
